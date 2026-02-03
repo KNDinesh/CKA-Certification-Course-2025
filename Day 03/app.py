8505420d51e39e59ae759ff19f6536b2889dfbf4
KNDinesh <127204193+KNDinesh@users.noreply.github.com>
2026-02-03 22:10:58 +0530

    Add comments for Flask app and Docker instructions
    Added comments explaining the Flask application and Docker usage.

--- Day 03/app.py
+++ Day 03/app.py
@@ -8,3 +8,6 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+# Flask is a light weight web framework for python that is widely used for building web applications and APIs. Code above is for a basic python flask application that says "Hello, Docker!". It listens on Port 5000.
+# We will containerize this application using Docker. Now to continue, go back to the content where Dockerfile is written in README.md.
