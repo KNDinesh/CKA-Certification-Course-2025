4b7974717489be05cf069730050f73b006e3442f
KNDinesh <127204193+KNDinesh@users.noreply.github.com>
2026-02-03 22:12:13 +0530

    Clarify Dockerfile reference in comments
    Updated comment to refer directly to Dockerfile for details.

--- Day 03/app.py
+++ Day 03/app.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
 
 # Flask is a light weight web framework for python that is widely used for building web applications and APIs. Code above is for a basic python flask application that says "Hello, Docker!". It listens on Port 5000.
-# We will containerize this application using Docker. Now to continue, go back to the content where Dockerfile is written in README.md.
+# We will containerize this application using Docker. Now to continue, go back to the Dockerfile to understand the steps and for more details related to Dockerfile refer README.md.
